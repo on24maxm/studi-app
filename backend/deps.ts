0b4d7e79cd4bae79af22c3610934f2963399f341
maximilianmalimonenko <maximilianmalimonenko@gmail.com>
2025-10-12 16:32:00 +0200

    add export and type for necessary dependencies

--- backend/deps.ts
+++ backend/deps.ts
@@ -1,3 +1,10 @@
-import { Application } from "https://deno.land/x/oak@v17.1.6/mod.ts";
-import { JWTPayload, jwtVerify, SignJWT } from "npm:jose@5.9.6";
-import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
+// Routing w/OAK
+export { Application, Router } from "https://deno.land/x/oak@v17.1.6/mod.ts";
+export type { RouterContext } from "https://deno.land/x/oak@v17.1.6/mod.ts";
+
+// JWT-Tokens Library
+export { jwtVerify, SignJWT } from "npm:jose@5.9.6";
+export type { JWTPayload } from "npm:jose@5.9.6";
+
+// Hash Password Library
+export { compare, hash } from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
